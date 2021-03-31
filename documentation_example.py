8a3a57bd538d76401d4cc62184ccb85f80bdc84c
SiriCamee <30780788+SiriCamee@users.noreply.github.com>
2021-03-31 11:53:28 +0200

    Update documentation_example.py

--- documentation_example.py
+++ documentation_example.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+#print headers of columns when present
 def x(a, print_columns=False):
+   '''
+   Args: excel_file for (a)
+   output: 
+   '''
    b = pd.read_excel(a)
    column_headers = list(b.columns.values)
    if print_columns:
